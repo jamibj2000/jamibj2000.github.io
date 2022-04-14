48a6db2f52d905462c829476aa88d91ca693dcc8
Antonio Alvarez <silentrakan@gmail.com>
2022-04-14 18:17:48 -0500

    loading files

---
+++ index.js
@@ -0,0 +1,37 @@
+import {upperCaseString} from './sweetalert_module.js'
+/* const saludo = upperCaseString('Holis, esto es una cadena de texto!')
+console.log(saludo); */
+
+
+/*
+  MENU - MODALS - ANIMATION INSPIRATED
+  DARK MODE
+  INSERT CV
+  MINIMALISTA
+
+*/
+
+/*
+  Servicio de mototaxi: Lista con tramos horarios en intervalos de 30 minutos, que inicie a las 8:30 am y termine a las 8:00 pm
+  e.g:  Bloque 1- 8:30 am
+        Bloque 2- 9:00 am
+        Bloque 3- 9:30 am
+        Hasta llegar a las 8:00 pm
+  Empresa tiene disponibilidad de 8 motociclistas cada 30 minutos, cuando alguien haga click sobre una de estas cajitas
+   deberá tomar un recurso de motociclistas (toma servicio de moto y descuenta en el conteo), Además de marcar la caja de alguna manera.
+   Si el mismo usuario clickea en la caja debería liberar el servicio de moto (no pasados ciertos segundos)
+*/
+
+/*
+  Trivias: Deberá ingresar algunos datos, luego podrá elegir una categoría de preguntas...
+  Enlazar con base de datos las preguntas de manera aleatoria con sus respectivas respuestas cifradas
+  Mostrar el tiempo de respuesta y mostrar resultados al finalizar la trivia
+*/
+
+/*
+  Crear un juego de memoria... anexar ranking global con tiempos de completar el juego y la cantidad de errores cometidos
+*/
+
+/*
+  Notificación por correo electrónico: ingresas tu panel administrativo
+*/
